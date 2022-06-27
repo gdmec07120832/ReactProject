f1060c3792cabe636fef505b5d62e0e0b897abca
tanchaoqiang <tanchaoqiang@linshimuye.com>
2022-06-27 17:18:06 +0800

    2022-06-27 修改了发布页面的布局

--- src/view/ViewReport/TechnicalOperation/ViewIndex_con2.js
+++ src/view/ViewReport/TechnicalOperation/ViewIndex_con2.js
@@ -4,7 +4,14 @@ import React,{Component} from "react";
 class ViewIndex_con2 extends Component{
   render(){
     return(
-      <div>bbb</div>
+      <div className="table-box">
+        <div className="TimeCss">
+          <p>数据时间：2022-06-16 至 2022-06-22</p>
+        </div>
+        <div className="CountCss">
+          <div>这里是发布页面</div>
+        </div>
+      </div>
     )
   }  
 }
